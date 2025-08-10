af6779b371c8344413937162d3206b9415bdfb32
Subham Sahu <subhambusiness286@gmail.com>
2025-08-10 17:29:33 +0530

    Changed

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,5 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
 import Backgrounds from "./Backgrounds";
 import Cards from "./Charecters";
 import Hero from "./Hero";
